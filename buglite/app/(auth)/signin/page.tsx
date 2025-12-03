b9b2440abc0fdbd196b0fe04b2469338c1a90cd8
DileepLakshan141 <dileeplakshan212@gmail.com>
2025-12-03 23:02:09 +0530

    signin page back video

--- buglite/app/(auth)/signin/page.tsx
+++ buglite/app/(auth)/signin/page.tsx
@@ -1,5 +1,20 @@
 const SigninPage = () => {
-  return <div className="w-full h-screen">signin page</div>;
+  return (
+    <div className="w-full h-screen relative">
+      {/* video container */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-full h-full absolute top-0 left-0"
+      >
+        <source src="/buglite_back1.mp4" type="video/mp4" />
+      </video>
+      {/* signins form */}
+      <div className="w-full h-screen absolute top-0 left-0"></div>
+    </div>
+  );
 };
 
 export default SigninPage;
