cebe3c4bf3b559a78fe6d353f547422184fddbca
DileepLakshan141 <dileeplakshan212@gmail.com>
2025-12-10 16:08:54 +0530

    modification to prisma client, project creation api route wired

--- buglite/prisma/prisma_client.ts
+++ buglite/prisma/prisma_client.ts
@@ -1,14 +1,9 @@
-import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error", "warn"],
-  });
-
-if (process.env.CURR_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({ adapter });
