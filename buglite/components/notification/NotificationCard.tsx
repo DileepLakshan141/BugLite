0e35f538675dcab53d6f9ab545f0f88ad70b462d
DileepLakshan141 <dileeplakshan212@gmail.com>
2025-12-21 09:57:48 +0530

    notification mark as read feature complete

--- buglite/components/notification/NotificationCard.tsx
+++ buglite/components/notification/NotificationCard.tsx
@@ -4,14 +4,42 @@ import { Separator } from "../ui/separator";
 import { Button } from "../ui/button";
 import dayjs from "dayjs";
 import { BadgeAlert, LaptopMinimalCheck, MailCheck } from "lucide-react";
-import { Badge } from "../ui/badge";
+import axios, { AxiosError } from "axios";
+import { toast } from "sonner";
+import { useState } from "react";
 
 const NotificationCard = ({ params }: { params: NOTIFICATION }) => {
   const { id, createdAt, title, message, status, issue_type } = params;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [readStatus, setReadStatus] = useState<boolean>(status);
+  const [closedIssue, setClosedIssue] = useState<boolean>(issue_type);
+
+  const markAsRead = async (notificationId: string) => {
+    try {
+      setLoading(true);
+      const response = await axios.put(
+        `/api/notifications/read/${notificationId}`
+      );
+      if (response.data.success) {
+        toast.success(response.data.message);
+        setReadStatus(true);
+        setClosedIssue(true);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      const wrapper = error as AxiosError<{ message: string }>;
+      toast.error(wrapper.response?.data.message);
+      console.log(wrapper);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="w-full border border-black rounded-lg my-2 p-2">
       <div className="flex items-center mb-2">
-        {issue_type ? (
+        {closedIssue ? (
           <LaptopMinimalCheck className="text-green-500 mr-3" />
         ) : (
           <BadgeAlert className="text-amber-500 mr-3" />
@@ -24,9 +52,15 @@ const NotificationCard = ({ params }: { params: NOTIFICATION }) => {
         <span className="text-sm text-muted-foreground italic">
           Received On: {dayjs(createdAt).format("HH:mm DD/MM/YYYY")}
         </span>
-        {!status ? (
-          <Button size="sm" variant="outline">
-            <MailCheck /> Mark as Read
+        {!readStatus ? (
+          <Button
+            size="sm"
+            variant="outline"
+            disabled={loading}
+            onClick={() => markAsRead(id)}
+          >
+            <MailCheck />
+            {loading ? "Marking..." : " Mark as Read"}
           </Button>
         ) : (
           ""
