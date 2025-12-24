333814b898a53ae7cbdf136e9890e43ca2fc22e6
DileepLakshan141 <dileeplakshan212@gmail.com>
2025-12-24 20:38:44 +0530

    minor modification

--- buglite/app/dashboard/home/page.tsx
+++ buglite/app/dashboard/home/page.tsx
@@ -43,10 +43,10 @@ const HomeScreenDashboard = () => {
     }
   };
 
-  useEffect(() => {
-    if (!userId) return;
-    fetchInsights();
-  }, [userId]);
+  // useEffect(() => {
+  //   if (!userId) return;
+  //   fetchInsights();
+  // }, [userId]);
   return (
     <div className="w-full flex flex-col justify-start items-start">
       <div className="w-full flex flex-col justify-start items-start mt-[90px] p-4">
